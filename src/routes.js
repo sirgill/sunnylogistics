e90d50342b8ed87fd639dcc96c19157b3c2754d7
Mini Kumari <minikumari878917@gmail.com>
2024-12-15 11:23:45 +0530

    "import fixes"

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from 'react-router-dom';
-import LandingPage from "./scenes/LandingPage/LandingPage";
+import LandingPage from "./scenes/Landingpage/LandingPage";
 import CarriersPage from "./scenes/Carriers/Carriers";
 import ShippersPage from "./scenes/Shippers/Shippers";
 import AboutPage from "./scenes/About/About";
